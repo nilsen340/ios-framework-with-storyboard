9d1916d5b4d0dcca6390451382bb21c9a1503daf
John Nilsen <nilsen340@gmail.com>
2016-03-25 10:53:05 -0400

    Initial state

---
+++ Example/Pods/Target Support Files/Pods-MyFramework_Tests/Pods-MyFramework_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyFramework_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MyFramework_Tests
+@end
---
+++ Pod/Classes/FrameworkVC.swift
@@ -0,0 +1,29 @@
+//
+//  FrameworkVC.swift
+//  Pods
+//
+//  Created by John Nilsen on 3/25/16.
+//
+//
+
+import UIKit
+
+class FrameworkVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
